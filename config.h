966fdcb39e7b82a3c196decabfb7d4dce0b56a6b
3Gigs <14863137+3Gigs@users.noreply.github.com>
2022-01-04 21:53:55 -0800

    Fix xresources

--- config.h
+++ config.h
@@ -7,6 +7,11 @@
  */
 static char *font = "JetBrains Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
+/* Spare fonts */
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -191,9 +196,9 @@ ResourcePref resources[] = {
 		{ "color13",      STRING,  &colorname[13] },
 		{ "color14",      STRING,  &colorname[14] },
 		{ "color15",      STRING,  &colorname[15] },
-		{ "background",   STRING,  &colorname[256] },
-		{ "foreground",   STRING,  &colorname[257] },
-		{ "cursorColor",  STRING,  &colorname[258] },
+		{ "background",   STRING,  &colorname[258] },
+		{ "foreground",   STRING,  &colorname[259] },
+		{ "cursorColor",  STRING,  &colorname[256] },
 		{ "termname",     STRING,  &termname },
 		{ "shell",        STRING,  &shell },
 		{ "minlatency",   INTEGER, &minlatency },
